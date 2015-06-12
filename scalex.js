cb9a85348cade83f7809d176869daaf447a0c05f
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2015-06-11 22:29:51 -0700

    .text(undefined) returns ""

--- scalex.js
+++ scalex.js
@@ -123,7 +123,14 @@ $(function () {
   }
 
   function renderResult(e) {
-    var r = $resultTpl.clone().find(".parent-class").text(e.parent.name).end().find(".parent-params").text(e.parent.typeParams).end().find(".name").text(e.name).end().find(".type-params").text(e.typeParams).end().find(".return").text(e.resultType).end().find(".package").text(e.package).toggleClass("special", e.package != "scala").end().find(".qualified-name").text(e.parent.qualifiedName).end();
+    var r = $resultTpl.clone()
+          .find(".parent-class").text(e.parent.name).end()
+          .find(".parent-params").text(e.parent.typeParams || "").end()
+          .find(".name").text(e.name).end()
+          .find(".type-params").text(e.typeParams || "").end()
+          .find(".return").text(e.resultType).end()
+          .find(".package").text(e.package || "").toggleClass("special", e.package != "scala").end()
+          .find(".qualified-name").text(e.parent.qualifiedName).end();
     if (e.valueParams) r.find(".params").text(e.valueParams);
     else r.find(".params-sep").remove();
     if (c = e.comment) {
